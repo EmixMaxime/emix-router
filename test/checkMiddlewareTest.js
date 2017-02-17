1bc1e57a47b676b7144e619a96eeff63f9f59e5d
EmixMaxime <emixmaxime@outlook.fr>
2017-02-17 19:30:22 +0100

    Delete the pending mark

--- test/checkMiddlewareTest.js
+++ test/checkMiddlewareTest.js
@@ -2,21 +2,24 @@ const chai = require('chai');
 const expect = require('chai').expect;
 const checkMiddleware = require('../src/utils/checkMiddleware');
 
-describe.skip('checkMiddleware', () => {
+function isEmix() {}
+
+describe('checkMiddleware', () => {
 
   it('It should return empty array with empty string | undefined', () => {
     const result = checkMiddleware();
     expect(result).to.be.instanceof(Array);
+    expect(result).to.be.deep.equal([]);
   });
 
   it('It should return an array with function parameter', () => {
     const result = checkMiddleware(isEmix);
-    expect(result).to.eql([isEmix]);
+    expect(result).to.deep.equal([isEmix]);
   });
 
   it('It should do nothing, just return my parameter', () => {
     const result = checkMiddleware([isEmix]);
-    expect(result).to.eql([isEmix]);
+    expect(result).to.deep.equal([isEmix]);
   });
 
 });
